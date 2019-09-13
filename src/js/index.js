b839f42805cb261a16730c4d9fa8f7bf7e772fd7
couch Janus <couchjanus@gmail.com>
2019-09-13 16:58:32 +0300

    added animation

--- src/js/index.js
+++ src/js/index.js
@@ -2,8 +2,8 @@
 import 'bootstrap';
 // import $ from "jquery";
 
-import './app';
+// import './app';
 // import './test';
-// import './jqtest';
+import './jqtest';
 // import './jqapp';
 // import './calc';
--- src/js/jqtest.js
+++ src/js/jqtest.js
@@ -2,28 +2,18 @@
 import $ from 'jquery';
 window.jQuery = window.$ = $;
 
-// $('.add-to-cart').on('click', function () {
-//     let y = 180;
-//     $('.card-img-top').css('transform','rotateY(' + y + 'deg)');
-// }); 
-
-// $(".add-to-cart").each(function(index, element){
-//     $(element).on('click', function (e) {
-//         let y = 180;
-//         $(e.target).parents('.card').find('img').css('transform','rotateY(' + y + 'deg)');
-//     });
-// });
-
-
-// $(".minus").click(function() {
-//     var val = parseInt($(this).siblings('.quontity').text());
-//     if (val > 1) {
-//         val = $(this).next().text(val-1);
-//     }
-// });
-
-// $(".plus").click(function() {
-//     var val = parseInt($(this).siblings('.quontity').text());
-//     $(this).prev().text(val+1);
-// });
-
+$(".add-to-cart").each(function(index, element){
+    $(element).on('click', function ()  {
+        // $(this).parents('.card').find('.win').show(1000);
+        // $(this).parents('.card').find('.win').show('fast');
+        // $(this).parents('.card').find('.win').show(1000, 'linear');
+        // $(this).parents('.card').find('.win').show('fast', ‘swing’);
+        // $(this).parents('.card').find('.win').toggle(1000);
+        // $(this).parents('.card').find('.win').fadeIn('fast', 'swing');
+        // $(this).parents('.card').find('.win').fadeTo('slow', 'swing');
+        $(this).parents('.card').find('.win').slideDown('slow', 'swing');
+        $(this).parents('.card').find('.win').slideUp('slow', 'linear');
+       
+    })
+});        
+ 
